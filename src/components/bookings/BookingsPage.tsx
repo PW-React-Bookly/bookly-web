a4733cc85aa6c310b0424c5a1b01a246fb3f563c
faliszewskii <igor.faliszewski1@gmail.com>
2023-01-06 01:40:03 +0100

    implement bootstrap and routing

--- src/components/bookings/BookingsPage.tsx
+++ src/components/bookings/BookingsPage.tsx
@@ -1,16 +1,16 @@
 import {useRecoilState} from "recoil";
 import {counterAtom} from "../../recoil/counterAtom";
 
-const MainView = () => {
+const BookingsPage = () => {
 
     const [counter, setCounter] = useRecoilState(counterAtom);
 
     return (
         <div>
-            <div>Hello Bookly! {counter>1 && 'x'+counter}</div>
+            <div>Hello Bookly from Bookings! {counter>1 && 'x'+counter}</div>
             <button onClick={()=>setCounter(counter+1)}>Hi</button>
         </div>
     )
 }
 
-export default MainView;
+export default BookingsPage;
