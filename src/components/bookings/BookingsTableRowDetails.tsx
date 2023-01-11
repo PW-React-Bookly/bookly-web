b905747925e6e195e64eadd5d19460ada2e6b71f
Filip <fillipser143@gmail.com>
2023-01-11 16:52:56 +0100

    Added BookingDetailsModal

--- src/components/bookings/BookingsTableRowDetails.tsx
+++ src/components/bookings/BookingsTableRowDetails.tsx
@@ -1,11 +1,26 @@
 import {BookingInterface} from "../../utils/interfaces/BookingInterface";
 import {dateToString} from "../../utils/interfaces/DateToString";
+import React, {useState} from "react";
+import {Button} from "react-bootstrap";
+import BookingDetailsModal from "./BookingDetailsModal";
 
 const BookingsTableRowDetails = (props: {booking: BookingInterface}) => {
+    const [modalShow, setModalShow] = useState(false);
+
+    const buttonStyle = {
+        display: 'flex',
+        gap: 10
+    }
     return (
         <div>
             <p>Period: {dateToString(props.booking.dateFrom)} - {dateToString(props.booking.dateTo)}</p>
-            <p>Price: {props.booking.price}</p>
+            <div style={buttonStyle}>
+                <p>Price: {props.booking.price}</p>
+                <Button onClick={() => setModalShow(true)}>
+                    Details
+                </Button>
+            </div>
+            <BookingDetailsModal booking={props.booking} show={modalShow} onHide={() => setModalShow(false)}/>
         </div>
     )
 }
