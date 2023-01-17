d14a02179d2f9890941128e46a53e7b7d90041bc
Jakub Górka <kubagorka27@gmail.com>
2023-01-17 16:46:20 +0100

    Implement user banning and booking cancelling

--- src/bookings/components/table/BookingCancelButton.tsx
+++ src/bookings/components/table/BookingCancelButton.tsx
@@ -1,11 +1,26 @@
 import {Button} from "react-bootstrap";
 import React from "react";
 import {BookingInterface} from "../../interfaces/bookingInterface";
+import {useRecoilState} from "recoil";
+import {refreshAtom} from "../../../common/recoil/refreshAtom";
 
 const BookingCancelButton = (props: {booking: BookingInterface}) => {
 
+    const [_, setRefresh] = useRecoilState(refreshAtom);
+
     const handleBookingCancel = () => {
-        throw new Error(`Not implemented, bookingId: ${props.booking.id}`);
+        const baseUrl = process.env.REACT_APP_BOOKLY_BACKEND_URL;
+        const endpointUrl = `/bookings/cancel/${props.booking.id}`;
+        const url = baseUrl + endpointUrl;
+
+        fetch(url, {
+            method : "POST",
+            mode: 'cors',
+        })
+            .then(() => {
+                setRefresh(Date.now());
+            })
+            .catch((error) => console.error(error));
     }
 
     return (
--- src/users/components/table/BanUserButton.tsx
+++ src/users/components/table/BanUserButton.tsx
@@ -1,9 +1,24 @@
 import { Button } from "react-bootstrap"
+import {useRecoilState} from "recoil";
+import {refreshAtom} from "../../../common/recoil/refreshAtom";
 
 const BanUserButton = (props: {userId: number}) => {
 
+    const [_, setRefresh] = useRecoilState(refreshAtom);
+
     const handleUserBan = () => {
-        throw new Error(`Not implemented, userId: ${props.userId}`);
+        const baseUrl = process.env.REACT_APP_BOOKLY_BACKEND_URL;
+        const endpointUrl = `/users/ban/${props.userId}`;
+        const url = baseUrl + endpointUrl;
+
+        fetch(url, {
+            method : "POST",
+            mode: 'cors',
+        })
+            .then(() => {
+                setRefresh(Date.now());
+            })
+            .catch((error) => console.error(error));
     }
 
     return (
